76376a312d7e9b18f1e17937cb5fa76bcf1e818c
2-Jet <SBAC>
2026-01-28 15:42:27 +0700

    V2

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3002;
+
+app.use(express.json());
+
+let department = [
+  { id: 1, name: "IT" },
+  { id: 2, name: "DBT" },
+];
 
 app.get("/", (req, res) => {
   res.send("Mr.Papangkorn Makwong");
 });
 
+app.get("/", (req, res) => {
+  res.send("department");
+});
+
+app.post("/department", (req, res) => {
+  let newDepartment = {
+    id: department.length + 1,
+    name: req.body.name,
+  };
+  department.push(newDepartment);
+  res.send(newDepartment);
+});
 app.listen(port, () => {
   console.log("Server Starting on port", port);
 });
